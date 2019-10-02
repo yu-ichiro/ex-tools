66ac1de7cde040334547327e73018fbca50bd0aa
Yuichiro Luke Smith <contact@yu-smith.com>
2019-10-02 16:05:25 +0900

    0.0.1

---
+++ ex_tools/__init__.py
@@ -0,0 +1,2 @@
+from .functools import *
+from .itertools import *
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ex-tools',
+    version='0.0.1',
+    description='Tools that include/extend functools/itertools +alpha',
+    url='git@github.com:yu-ichiro/ex-tools.git',
+    author='Yuichiro Luke Smith',
+    author_email='yuichiro.luke@gmail.com',
+    license='unlicense',
+    packages=['ex_tools'],
+    zip_safe=False
+)
