9b73004a1c5b7a3a51d664a59bb048ffc7754ff7
Yuichiro Luke Smith <contact@yu-smith.com>
2019-11-06 16:49:55 +0900

    dist 0.1.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='ex-tools',
-    version='0.1.0',
+    version='0.1.1',
     description='Tools that include/extend functools/itertools +alpha',
     url='https://github.com/yu-ichiro/ex-tools',
     author='Yuichiro Luke Smith',
     author_email='yuichiro.luke@gmail.com',
     license='MIT',
-    packages=['ex_tools'],
+    packages=['ex_tools', 'ex_tools.functools', 'ex_tools.itertools'],
     zip_safe=False
 )
